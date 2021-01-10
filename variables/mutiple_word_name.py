2f469c205fa0a3c2ba2dce784a607dd689b77b93
PhumiphatAtom <atom_li1@hotmail.com>
2021-01-10 11:11:20 +0700

    create new folder

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,10 @@
+# Camel Case
+myVariableName = "Phumiphat Li"
+
+# Pascal Case
+MyVariableName = "Phumiphat Li"
+
+# Snake Case
+my_variable_name = "Phumiphat Li"
+
+ืnumber_list = [1,2,3]
---
+++ variables/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "Atom"
+my_var = "Atom"
+_my_var_ = "Atom"
+myVar = "Atom"
+MYVAR = "Atom"
+myvar2 = "Atom"
+
+#ไม่สามารถประกาศได้
+2myvar = "Atom"
+my-var= "Atom"
+my var = "Atom"
